28c16d0d8d4b803ff7e533097b8848af0e1627e6
Rebecca Ackerman <rcackerman@gmail.com>
2018-05-16 17:12:01 -0400

    Parsing sql so that SQLAlchemy will work

--- audit/test.py
+++ audit/test.py
@@ -4,26 +4,39 @@ import glob
 import pandas
 import numpy
 from sqlalchemy import create_engine
-from sqlalchemy.schema import CreateSchema, DropSchema
+from sqlalchemy.schema import CreateSchema, DropSchema, Table
+from sqlalchemy.sql import text
 from setup import *
+
+def parse_sql(filename):
+    """Parses SQL into a list of queries, so SQLAlchemy can deal.
+    """
+    with open(filename, 'r') as f:
+        queries = f.read().split(';')
+    return([q for q in queries if q.strip() != ''])
+
 
 with ENGINE.connect() as conn:
     setup(conn)
 
+
+sql_statements = parse_sql('..\case_population.sql')
 # run
 conn = ENGINE.connect()
 conn.execute('set search_path to audit')
-conn.execute() # survey sql
-conn.close()
+for s in sql_statements:
+    conn.execute(s)
 
-# get case ids
+cases = conn.execute(
+            'select cas_file_number, cas_aliasid from survey_cases').fetchall()
+results = conn.execute(text(
+                        "select eligible_case, eligible_pers, cas_file_number "
+                        "from cases where cas_file_number in :numbers"),
+                       numbers = tuple([i[0] for i in ids])
+                       ).fetchall()
 
-# check columns
 
-# check whether case is eligible
-
-# check whether person is eligible
 
 # teardown
-with ENGINE.connect() as conn:
-    teardown(conn)
+teardown(conn)
+conn.close()
