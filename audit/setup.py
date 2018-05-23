c553ec33386fbb03874ee7b56a2587bee1311a4c
Rebecca Ackerman <rcackerman@gmail.com>
2018-05-23 11:58:43 -0400

    Moving testing utility functions to setup.py

--- audit/setup.py
+++ audit/setup.py
@@ -18,6 +18,22 @@ def load_df(filename, **kwargs):
     return frame
 
 
+def parse_sql(filename):
+    """Parses SQL into a list of queries, so SQLAlchemy can deal.
+    """
+    with open(filename, 'r') as f:
+        queries = f.read().split(';')
+    return([q for q in queries if q.strip() != ''])
+
+
+def execute_survey_sql(conn, sql_file, tablename):
+    sql_statements = parse_sql(sql_file)
+    conn.execute('set search_path to audit')
+    for s in sql_statements:
+        conn.execute(s)
+    return True
+
+
 def setup(conn): 
     """Set up testing database.
     """
@@ -28,8 +44,60 @@ def setup(conn):
                           con=conn,
                           schema='audit',
                           if_exists = 'replace')
+    return True
+
 
 def teardown(conn):
     """Teardown testing database.
     """
     conn.execute(DropSchema('audit', cascade=True))
+    return True
+
+
+def get_intermediary_test_results(conn, pdcms_table, survey_table):
+    """Compare the results against the eligiblity status stated in the test
+    tables.
+
+    Arguments:
+    conn:           connection object
+    results:        records in the survey_* table being tested
+    pdcms_table:    SQLAlchemy Table object reflecting the (fake) PDCMS table
+    survey_table:   SQLAlchemy Table object reflecting the survey table 
+    """
+    if survey_table.name == 'survey_cases':
+        id_col = 'cas_file_number'
+        elig_col = 'eligible_case'
+    elif survey_table.name == 'survey_names':
+        id_col = 'nam_nameid'
+        elig_col = 'eligible_pers'
+    else:
+        return False
+
+    results = select([survey_table.c[id_col]])
+
+    s_falsepos = select([pdcms_table.c[id_col],
+                         pdcms_table.c.eligible_case,
+                         pdcms_table.c.eligible_pers
+                        ]).where(pdcms.c[elig_col].ilike('no%')) \
+                          .where(pdcms_table.c[id_col].in_(results))
+    s_falseneg = select([pdcms_table.c[id_col],
+                         pdcms_table.c.eligible_case,
+                         pdcms_table.c.eligible_pers
+                        ]).where(pdcms_table.c[elig_col].ilike('yes%')) \
+                          .where(pdcms_table.c[id_col].notin_(results))
+
+    return {'false positives': conn.execute(s_falsepos).fetchall(),
+            'false negatives': conn.execute(s_falseneg).fetchall()}
+
+
+def get_final_test_results(sql_table_df, desired_result_df):
+    """Compare the final postgresql survey table against the desired result.
+    For ease of re-use, takes two pandas dataframes.
+    
+    Arguments:
+    sql_table_df:       Pandas DataFrame object
+    desired_result_df:  Pandas DataFrame object
+    """
+    pass
+
+
--- audit/test.py
+++ audit/test.py
@@ -8,71 +8,37 @@ from sqlalchemy.sql import select, text
 from sqlalchemy.schema import CreateSchema, DropSchema
 from setup import *
 
-def parse_sql(filename):
-    """Parses SQL into a list of queries, so SQLAlchemy can deal.
-    """
-    with open(filename, 'r') as f:
-        queries = f.read().split(';')
-    return([q for q in queries if q.strip() != ''])
 
-
-def get_result_comparison(conn, pdcms_table, survey_table):
-    """Compare the results against the eligiblity status stated in the test
-    tables.
-
-    Arguments:
-    conn:           connection object
-    results:        records in the survey_* table being tested
-    pdcms_table:    SQLAlchemy Table object reflecting the (fake) PDCMS table
-    survey_table:   SQLAlchemy Table object reflecting the survey table 
-    """
-    if survey_table.name == 'survey_cases':
-        id_col = 'cas_file_number'
-        elig_col = 'eligible_case'
-    elif survey_table.name == 'survey_names':
-        id_col = 'nam_nameid'
-        elig_col = 'eligible_pers'
-    else:
-        return False
-
-    results = select([survey_table.c[id_col]])
-
-    s_falsepos = select([pdcms_table.c[id_col],
-                         pdcms_table.c.eligible_case,
-                         pdcms_table.c.eligible_pers
-                        ]).where(pdcms.c[elig_col].ilike('no%')) \
-                          .where(pdcms_table.c[id_col].in_(results))
-    s_falseneg = select([pdcms_table.c[id_col],
-                         pdcms_table.c.eligible_case,
-                         pdcms_table.c.eligible_pers
-                        ]).where(pdcms_table.c[elig_col].ilike('yes%')) \
-                          .where(pdcms_table.c[id_col].notin_(results))
-
-    return {'false positives': conn.execute(s_falsepos).fetchall(),
-            'false negatives': conn.execute(s_falseneg).fetchall()}
 
 
 with ENGINE.connect() as conn:
     setup(conn)
 
+pdcms_tables = MetaData(bind=ENGINE, schema='audit')
 
-def test_cases():
-    sql_statements = parse_sql('..\case_population.sql')
+# run
+conn = ENGINE.connect()
+conn.close()
 
-    # run
-    conn = ENGINE.connect()
-    conn.execute('set search_path to audit')
-    for s in sql_statements:
-        conn.execute(s)
+def test_case_pop_sql():
+    """Test that only eligible cases are returned.
+    """
+    execute_survey_sql(conn, '..\case_population.sql')
 
-    cases = Table('cases', meta, autoload=True, autoload_with=ENGINE,
-                  schema='audit')
+    # define the survey table in SQL alchemy
     survey_cases = Table('survey_cases', meta, autoload=True, autoload_with=ENGINE,
                   schema='audit')
     print(get_result_comparison(conn, cases, survey_cases))
 
 
-    conn.close()
+def test_people_pop_sql():
+    """Test that only eligible people are returned.
+    """
+    execute_survey_sql(conn, '..\people_population.sql')
+    
+    # define the survey table in SQL alchemy
+    survey_people = Table('survey_people', meta, autoload=True, autoload_with=ENGINE,
+                          schema='audit')
 
 
 # teardown
