66bb0f55c2b0f0faa61b42ea0efe506df469747f
Rebecca Ackerman <rcackerman@gmail.com>
2018-08-29 15:50:04 -0400

    Since we're only surveying a small number of people, we don't need to get their addresses and phone numbers

--- population.sql
+++ population.sql
@@ -1,51 +1,17 @@
--- client_addresses.sql
+-- population.sql
 
 -- Get first and last name for each client in a person grouping
-WITH
-
-client_names AS (
-  SELECT
-    person_id,
+SELECT
     nam_nameid,
-    nam_first_name AS first_name,
-    nam_last_name AS last_name,
-    nam_nysid AS nysid,
+    person_id,
+    array_agg(DISTINCT nam_first_name || nam_last_name) AS client_names,
     dob,
     language,
     gender,
     ethnicity,
-    citizenship
+    citizenship,
+    array_agg(DISTINCT nam_citizenship_status) AS citizenship_statuses
 FROM survey_people
 JOIN names
   ON survey_people.person_id = names.nam_alias_link
-),
-
--- Get case information
-client_cases AS (
-  SELECT
-    person_id,
-    nam_nameid,
-    first_name,
-    last_name,
-    nysid,
-    dob,
-    language
-)
-
--- Get all addresses for a given alias set.
--- This will get us a row for every address a client had when their case started
-SELECT DISTINCT
-  client_names.*,
-  COALESCE(cas_open_date::varchar, adr_date) AS adr_date, -- sometimes addresses don't have dates, so fall back on case open date
-  adr_type,
-  adr_street1,
-  adr_street2,
-  adr_city,
-  adr_state,
-  adr_zipcode,
-  adr_ph_number,
-  adr_more_phones
-FROM clients_filtered
-JOIN addresses
-  ON nam_nameid = adr_nameid
 ;
