cd3f5faf1d4886942f29f259644701a44c4fdaeb
Rebecca Ackerman <rcackerman@gmail.com>
2018-08-31 15:16:39 -0400

    Putting everything together in an easy-to-read way

--- population.sql
+++ population.sql
@@ -2,16 +2,12 @@
 
 -- Get first and last name for each client in a person grouping
 SELECT
-    nam_nameid,
-    person_id,
-    array_agg(DISTINCT nam_first_name || nam_last_name) AS client_names,
-    dob,
-    language,
-    gender,
-    ethnicity,
-    citizenship,
-    array_agg(DISTINCT nam_citizenship_status) AS citizenship_statuses
+    array_agg(DISTINCT nam_first_name || nam_last_name ORDER BY nam_last_name, nam_first_name) AS client_names,
+    survey_people.*,
+    survey_cases.*
 FROM survey_people
 JOIN names
   ON survey_people.person_id = names.nam_alias_link
+JOIN survey_cases
+  ON survey_people.person_id = survey_cases.cas_aliasid
 ;
