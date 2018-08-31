195cdda756fc537d7575a575651d96f6534a3f33
Rebecca Ackerman <rcackerman@gmail.com>
2018-08-31 15:33:48 -0400

    Correctly deduplicating and aggregating full names

--- population.sql
+++ population.sql
@@ -1,13 +1,28 @@
 -- population.sql
 
+WITH fullnames AS (
 -- Get first and last name for each client in a person grouping
+  SELECT
+    nam_alias_link,
+    array_agg(fname ORDER BY fname) AS client_names
+  FROM (
+    SELECT distinct
+      nam_alias_link,
+      nam_first_name || ' ' || nam_last_name AS fname
+    FROM names
+  ) nams
+  group BY nam_alias_link
+)
+
 SELECT
-    array_agg(DISTINCT nam_first_name || nam_last_name ORDER BY nam_last_name, nam_first_name) AS client_names,
+    client_names,
     survey_people.*,
     survey_cases.*
 FROM survey_people
-JOIN names
-  ON survey_people.person_id = names.nam_alias_link
+JOIN fullnames
+  ON survey_people.person_id = nam_alias_link
 JOIN survey_cases
   ON survey_people.person_id = survey_cases.cas_aliasid
+LEFT JOIN sentences
+  ON survey_cases.cas_file_number = sentences.snt_file_number
 ;
