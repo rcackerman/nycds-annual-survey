c3d95f848deb672063fca2b0b2dfb3ba299ed0fa
Rebecca Ackerman <rcackerman@gmail.com>
2018-08-31 15:09:46 -0400

    Removing unnecessary sentence and disposition information to keep from duplicating records

--- case_population.sql
+++ case_population.sql
@@ -1,4 +1,5 @@
--- population.sql
+-- case_population.sql
+-- Get all cases that are eligible to be picked
 
 DROP TABLE IF EXISTS survey_cases;
 
@@ -18,60 +19,46 @@ CREATE TABLE survey_cases AS
       cas_fc_number,
       cas_fc_short,
       cas_docket,
-      cas_indictment,
-      dsp_action,
-      dsp_date,
-      dsp_notes,
-      CASE
-        WHEN dsp_action in ('ACD', 'MACD', 'FACD')
-          THEN true
-        ELSE
-          false
-      END AS dsp_ACD,
-      CASE
-        WHEN dsp_action in ('ADBR', 'ADBX', 'ADQU', 'ADRI', 'ADSA', 'AFC',
-                            'C/I', 'EXH', 'INEL', 'PROS', 'R18B', 'RELC',
-                            'RHOM', 'RLAS', 'RNDS', 'RPC')
-          THEN true
-        ELSE
-          false
-      END AS dsp_transfer
+      cas_indictment
     FROM cases
-    LEFT JOIN dispositions
-      ON cas_file_number = dsp_file_number
-      AND dsp_action IS NOT null AND dsp_action != ''
     WHERE cas_case_status = 'C'
     AND cas_closed_date::DATE > current_date - '6 months'::INTERVAL
     AND cas_closed_date::DATE <= current_date
   ),
 
-  -- Remove clients with current case.
-  -- Remove clients with disposition of being transferred to another provider.
-  clients_no_current_case AS (
+  cases_w_clients_without_current_case AS (
+  -- Remove clients if they or any of their aliases have an open case.
     SELECT
       *
     FROM closed_cases
-    WHERE dsp_transfer = 'f'
-    AND NOT EXISTS (
+    WHERE NOT EXISTS (
       SELECT 1
       FROM cases
       WHERE cases.cas_case_status != 'C'
       AND cases.cas_aliasid = closed_cases.cas_aliasid
     )
-    -- AND NOT EXISTS (
-      -- SELECT 1
-      -- FROM closed_cases AS self
-      -- WHERE self.dispo_type = 'ACD'
-      -- AND self.cas_aliasid = closed_cases.cas_aliasid
-      -- AND self.cas_file_number != closed_cases.cas_file_number
-    -- )
   ),
 
-  -- Remove clients who were ever 730'd
-  clients_never_730d AS (
+  cases_not_transferred AS (
+  -- Remove case if it had a transfer disposition
     SELECT
       *
-    FROM clients_no_current_case
+    FROM cases_w_clients_without_current_case
+    WHERE NOT exists (
+      SELECT 1
+      FROM dispositions
+      WHERE dsp_file_number = cas_file_number
+      AND dsp_action IN ('ADBR', 'ADBX', 'ADQU', 'ADRI', 'ADSA', 'AFC',
+                            'C/I', 'EXH', 'INEL', 'PROS', 'R18B', 'RELC',
+                            'RHOM', 'RLAS', 'RNDS', 'RPC')
+      )
+  ),
+
+  cases_with_clients_never_730d AS (
+  -- Remove cases with clients who were ever 730'd or found not guilty by reason of insanity
+    SELECT
+      *
+    FROM cases_not_transferred
     WHERE NOT EXISTS (
       SELECT 1
       FROM (
@@ -86,16 +73,39 @@ CREATE TABLE survey_cases AS
         WHERE dsp_action = '730'  
           OR dsp_action = 'NGMD'
       ) ever_mi
-      WHERE clients_no_current_case.cas_aliasid = ever_mi.cas_aliasid
+      WHERE cases_not_transferred.cas_aliasid = ever_mi.cas_aliasid
     )
   )
 
-  SELECT
-    clients_never_730d.*,
-    snt_type,
-    snt_condition,
-    snt_notes
-  FROM clients_never_730d
-  LEFT JOIN sentences
-    ON cas_file_number = snt_file_number
+  SELECT distinct
+    cas_file_number,
+    cas_clientid,
+    cas_aliasid,
+    cas_open_date,
+    cas_closed_date,
+    cas_case_type,
+    cas_case_detail,
+    cas_tc_number,
+    cas_tc_short,
+    cas_fc_number,
+    cas_fc_short,
+    cas_docket,
+    cas_indictment,
+    only_acd,
+    case_dismissed
+  FROM cases_with_clients_never_730d
+  LEFT JOIN (
+    SELECT
+      dsp_file_number,
+      MIN(CASE
+        WHEN dsp_action in ('ACD', 'MACD', 'FACD') THEN 1
+        ELSE 0 END) AS only_acd,
+      MIN(CASE
+        WHEN dsp_action like 'DIS%' THEN 1
+        ELSE 0 END) AS case_dismissed
+    FROM dispositions
+    WHERE dsp_action != '' AND dsp_action IS NOT NULL
+    group BY dsp_file_number
+  ) disps
+    ON cas_file_number = dsp_file_number
 ;  
